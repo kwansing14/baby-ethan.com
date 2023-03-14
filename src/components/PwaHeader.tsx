f4b51f2c55ce1fe8dbe1d6c48aa92a2a1e762635
kwansing14 <kwansing@hotmail.sg>
2023-03-14 14:27:07 +0800

    fix pwa

--- src/components/PwaHeader.tsx
+++ src/components/PwaHeader.tsx
@@ -42,7 +42,7 @@ const PwaHeader = () => {
         sizes="16x16"
         href="/fanvicon/favicon-16x16.png"
       />
-      <link rel="manifest" href="favicon/site.webmanifest" />
+      <link rel="manifest" href="/manifest.json" />
       <link
         rel="mask-icon"
         href="/favicon/safari-pinned-tab.svg"
