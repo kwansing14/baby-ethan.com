4e975e8cce960156348740919e1e3a5cfaed8dbc
kwansing14 <kwansing@hotmail.sg>
2023-03-14 14:31:53 +0800

    fix manifest json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,7 +67,7 @@ const Home = (
           sizes="16x16"
           href="/favicon/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        {/* <link rel="manifest" href="/manifest.json" /> */}
         <link
           rel="mask-icon"
           href="/favicon/safari-pinned-tab.svg"
